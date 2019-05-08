ca920937953c66eb5276be92ae869c8ec4416da6
Mahmoud <Mahmoud@Mahmouds-MacBook-Air.local>
2019-05-08 23:51:40 +0200

    =support preparation (ready to run), online, and offline

--- build_npy.py
+++ build_npy.py
@@ -1,7 +1,7 @@
 import os
 
-show = "python tools/generate_detections.py --model=resources/networks/mars-small128.pb \
+gen_detection_cmd = "python tools/generate_detections.py --model=resources/networks/mars-small128.pb \
 --mot_dir=./MOT16/train \
 --output_dir=./resources/detections/MOT16_train"
 
-os.system(show)
+os.system(gen_detection_cmd)
--- run.py
+++ run.py
@@ -1,19 +1,31 @@
 import os
+import json
+
+
+os.system("echo 'running all....'")
+advance_count = 1
+
+
+with open('app.config') as data:
+    config = json.load(data)
+    print(config)
 # before running the file make sure:
 # make sure that --model points to the correct network file such as mars-small128.pb 
 # make sure that --sequence_dir points to the sequence images such as MOT16-13 and it associated detections in the next flag
 # make sure that --detection_file points to the correct npy file
+config["mode"] = "deep_sort_app.py"
+if config["mode"] == "online":
+	config["mode"] = "deep_sort_app_online.py"
 
+show = "python {} \
+    --sequence_dir={} \
+    --detection_file={} \
+    --output_file={} \
+    --min_confidence={} \
+    --nn_budget={} \
+    --display={} \
+    --record_file={}".format(config["mode"], config["sequence_dir"], config["detection_file"], config["output_file"], config["min_confidence"], config["nn_budget"], config["display"], config["record_file"])
 
-show = "python deep_sort_app.py \
-	--model=resources/networks/mars-small128.pb \
-    --sequence_dir=./MOT16/train/MOT16-13 \
-    --detection_file=./resources/detections/MOT16_train/MOT16-13.npy \
-    --output_file=./resources/detections/MOT16_records/MOT16-13.txt \
-    --min_confidence=0.3 \
-    --nn_budget=100 \
-    --display=True \
-    --record_file=x.avi"
 os.system(show)
 
 
