88263af8e3034af6704ff4bb1fb67004b1fbb0d8
Mahmoud <Mahmoud@Mahmouds-MacBook-Air.local>
2019-05-09 14:26:50 +0200

    =updated links and minor fixes

--- build_npy.py
+++ build_npy.py
@@ -9,7 +9,7 @@ with open('app.config') as data:
     config = json.load(data)
     # print(config)
 
-    
+
 # gen_detection_cmd = "python tools/generate_detections.py --model=resources/networks/mars-small128.pb \
 # --mot_dir=./MOT16/train \
 # --output_dir=./resources/detections/MOT16_train"
@@ -17,7 +17,7 @@ with open('app.config') as data:
 
 gen_detection_cmd = "python tools/generate_detections.py --model={} \
 --mot_dir={} \
---output_dir={}".formate(config["model"], config["mot_dir"], config["output_dir"])
+--output_dir={}".format(config["model"], config["mot_dir"], config["output_dir"])
 
 
 
