4733827841d5a98d27fc66e783533fcf013e9471
Mahmoud <Mahmoud@Mahmouds-MacBook-Air.local>
2019-06-06 01:27:42 +0200

    =updating readme

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ advance_count = 1
 with open('app.config') as data:
     config = json.load(data)
 
-online_sort_app = "python deep_sort_app_online.py \
+online_sort_command = "python deep_sort_app_online.py \
 	--frozen={}\
 	--model={}\
     --input_video={} \
@@ -20,11 +20,12 @@ online_sort_app = "python deep_sort_app_online.py \
     --min_confidence={} \
     --nn_budget={} \
     --record_video={}".format(config['d_model'], config["f_model"], config["input_video"], int(config['frame_rate']), float(config["detection_threshold"]), config["min_confidence"], config["nn_budget"], config["record_video"])
-
-os.system(online_sort_app)
+os.system(online_sort_command)
 
 os.system("echo '\nsaving video results'")
-save_video_results(config['input_video'])
+save_results_command = "python save_results.py --source_video={}".format(config['input_video'])
+os.system(save_results_command)
+
 
 
 
